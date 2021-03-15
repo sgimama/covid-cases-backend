c1b8293717f1f95985071598a6548aef2ace76b5
Sergio Gimenez <36163163+sgimama@users.noreply.github.com>
2021-03-15 22:04:54 +0100

    fix error heroku

--- App/routes/slotMachine/api.js
+++ App/routes/slotMachine/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const slotMachineController = require("../../controllers/slotMachineController");
+const slotMachineController = require("../../controllers/SlotMachineController");
 const auth = require("../../middlewares/Auth");
 
 router.post("/pull", auth, slotMachineController.pull);
